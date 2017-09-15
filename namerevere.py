f31f8e98e78d6704d849fabe72e00a377ccdaef5
Amit943 <amitanand2@outlook.com>
2017-09-15 15:08:56 -0700

    print name in reverse order

---
+++ namerevere.py
@@ -0,0 +1,15 @@
+import time
+
+
+first_Name=str(input("Please enter your first name :"))
+last_name =str(input("Please enter your last name :"))
+final_Name=first_Name + last_name
+print(final_Name)
+print("Now reversing the string :")
+time.sleep(2)
+print(final_Name[::-1])
+
+
+
+
+
