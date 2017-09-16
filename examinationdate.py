ed09ddc64a30772cb0daf3238a679d9bb2a69565
Amit943 <amitanand2@outlook.com>
2017-09-16 14:31:59 -0700

    print examination date in a particular format

---
+++ examinationdate.py
@@ -0,0 +1,6 @@
+import time
+
+exam_st_date = (11, 12, 2014)
+print("Retrieving date for examination ...")
+time.sleep(1)
+print("The exmination will start from :",exam_st_date[0],"/",exam_st_date[1],"/",exam_st_date[2])
