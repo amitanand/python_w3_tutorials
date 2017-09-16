36ebd9c357bc70ff1e305180bdba34e637af0ccf
Amit943 <amitanand2@outlook.com>
2017-09-16 14:32:20 -0700

    check even & odd

---
+++ evenodd.py
@@ -0,0 +1,11 @@
+import time
+
+n=int(input("Please enter the number to check :"))
+if n%2 ==0:
+    print("Checking number ...")
+    time.sleep(1)
+    print(" The number you entered is even ")
+else:
+    print("Checking number ...")
+    time.sleep(1)
+    print("The number you entered is odd")
