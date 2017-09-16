8e2d314b29ddcde8f0306ff9a7967ce93ba54ea2
Amit943 <amitanand2@outlook.com>
2017-09-16 14:31:29 -0700

    print first & last element of the list

---
+++ firstlastlist.py
@@ -0,0 +1,10 @@
+import time
+
+color_list = ["Red","Green","White" ,"Black"]
+print("Retrieving the first element from the list ...")
+time.sleep(1)
+print("First element is :",color_list[0])
+time.sleep(1)
+print("Retrieving the last element from the list ...")
+time.sleep(1)
+print("Last element is",color_list[3])
