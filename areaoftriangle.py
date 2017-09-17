a0023567ec9c96eac29f51b370038bece2b2a1c7
Amit Anand <anand.amit943@gmail.com>
2017-09-16 18:21:13 -0700

    area if a triangle

---
+++ areaoftriangle.py
@@ -0,0 +1,8 @@
+import time
+
+base=float(input("Please enter the base of the triangle :"))
+height = float(input("Please enter the height of the triangle :"))
+print("Calculating area of triangle on the basis of your input ...")
+area=(0.5)*base*height
+time.sleep(1)
+print("AREA :",area)
