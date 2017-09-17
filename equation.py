5cb62ad69034aee9798dca454cc8cc8ba6331ff3
Amit Anand <anand.amit943@gmail.com>
2017-09-17 12:09:42 -0700

    square value of x+y

---
+++ equation.py
@@ -0,0 +1,9 @@
+import time
+
+def quadratic(x,y):
+    print("Calculating value of the equation (x+y)*(x+y) :")
+    time.sleep(1)
+    output=(x+y)*(x+y)
+    print("Output :",output)
+
+quadratic(3,4)
